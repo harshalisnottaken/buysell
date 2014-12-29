c3e685cb951fcfd7d029b21bad912cfd9a21c386
Kiran <Kiran_Ramreddy@gap.com>
2014-12-29 17:54:53 +0530

    Kiran/Sham : Added jackson api on build.gradle and updated applicationContext.xml accordingly

--- src/main/java/com/bs/controller/UserRegistrationController.java
+++ src/main/java/com/bs/controller/UserRegistrationController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bs.domain.User;
 import com.bs.service.UserRegistrationService;
@@ -20,7 +21,8 @@ public class UserRegistrationController {
 	@Autowired
 	private UserRegistrationService userRegistrationService;
 	
-	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	@RequestMapping(value="/register-user", method = RequestMethod.POST)
+	@ResponseBody
 	public ResponseEntity<?> registerUser(@RequestBody User user) {
 		log.info("Registering User details");
 		userRegistrationService.registerUser(user);
